4d0d9cb7595d3378f5b0c2919b3c7b115a690364
andy-aguilar <aaguil3@gmail.com>
2020-06-29 17:45:58 -0400

    removed unused methods from ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,15 +3,5 @@ class Ingredient < ApplicationRecord
     has_many :recipes, through: :recipe_ingredients
     has_many :allergies
     has_many :users, through: :allergies
-
-
-    def self.sort_by_allergies
-        self.all.sort_by do |ingredient|
-            ingredient.allergy_count
-        end.reverse!
-    end
-    def allergy_count
-        self.allergies.count
-    end
     
 end
