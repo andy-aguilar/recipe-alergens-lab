ce5e5c89c2f6df4cc9c91ba1110f14a7b6543702
andy-aguilar <aaguil3@gmail.com>
2020-06-29 12:59:39 -0400

    created controllers and views for ingredients and recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,14 +11,15 @@ class RecipesController < ApplicationController
         @recipe = Recipe.new
     end
 
-    # def create 
-    #     @recipe = Recipe.create(recipe_params)
-    # end
+    def create 
+        @recipe = Recipe.create(recipe_params)
+        redirect_to recipe_path(@recipe)
+    end
 
     private
 
-    # def recipe_params
-    #     params.require(:recipe).permit(?)
-    # end
+    def recipe_params
+        params.require(:recipe).permit(:name, :user_id)
+    end
     
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,13 @@ class Ingredient < ApplicationRecord
     has_many :recipes, through: :recipe_ingredients
     has_many :allergies
     has_many :users, through: :allergies
+
+
+    def self.order_by_allergy
+        Ingredient.order(:allergy_count)
+    end
+
+    def allergy_count
+        self.allergies.count
+    end
 end
